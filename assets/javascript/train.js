98cd92c4e7aeb9b4cf5e53707f14179589e946e6
ECarlson <ericcarlson4@outlook.com>
2019-04-22 10:24:27 -0700

    need to fix some massive errors

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -24,19 +24,20 @@ document.querySelector("#train-stuff").addEventListener("submit" , function(even
     var frequency = document.querySelector("#frequency-input").value.trim();
 
     console.log(trainColor , trainLine , firstTrain , frequency);
+    //This uses the reference of the database and pushes the values
+    //database.ref().push({
+        //color: trainColor,
+        //trainline: trainLine,
+        //firstTrain: firstTrain,
+        //frequency: frequency,
+    //});
+    //This should clear out the values of the form
+    document.querySelector("#train-color-input").value = "";
+    document.querySelector("#train-line-input").value = "";
+    document.querySelector("#first-train-input").value = "";
+    document.querySelector("#frequency-input").value = "";
 
-
-
-
-
-
-
-
-
-
-
-
-
+    return false;
 
 });
 
