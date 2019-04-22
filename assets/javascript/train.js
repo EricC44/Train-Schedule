08f6af40cfd085ae3d9e0f4a9818b02a97ca57db
ECarlson <ericcarlson4@outlook.com>
2019-04-21 17:09:13 -0700

    Initialized Firebase

---
+++ assets/javascript/train.js
@@ -0,0 +1,15 @@
+//Set the initial index value to 0
+
+var index = 0;
+
+//This is to initialize firebase 
+var config = {
+    apiKey: "AIzaSyDmbrJY8SYIoakszgztcjviDIpcPrqXPnY",
+    authDomain: "train-schedule-bart.firebaseapp.com",
+    databaseURL: "https://train-schedule-bart.firebaseio.com",
+    projectId: "train-schedule-bart",
+    storageBucket: "train-schedule-bart.appspot.com",
+    messagingSenderId: "145812291444"
+  };
+  firebase.initializeApp(config);
+
